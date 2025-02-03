67a0bed3c1df6defdba9dc6adcf38f9d0f7f2f52
Jana-script <jana.jscript@gmail.com>
2025-02-03 20:09:55 +0100

    App layout and Routes

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -4,16 +4,23 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="product" element={<Product />} />
+          <Route index element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="product" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>LIST</p>} />
+            <Route path="cities" index element={<p>cities</p>} />
+            <Route path="countries" element={<p>countries</p>} />
+            <Route path="form" element={<p>form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
