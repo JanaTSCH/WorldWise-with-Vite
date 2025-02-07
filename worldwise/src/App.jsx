c202b3eeccb9276c891685adf78f5cfcda46d833
Jana-script <jana.jscript@gmail.com>
2025-02-07 16:20:12 +0100

    FakeAuth
    Protecting a Route

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -12,6 +12,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtecteedRoute from "./pages/ProtecteedRoute";
 
 function App() {
   return (
@@ -23,7 +24,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtecteedRoute>
+                  <AppLayout />
+                </ProtecteedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" index element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
