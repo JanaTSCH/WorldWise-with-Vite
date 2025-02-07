87efacbffae5e646da0e970a95bc8e50ff667827
Jana-script <jana.jscript@gmail.com>
2025-02-07 12:20:39 +0100

    Fake Authentication
    Setting up context

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,14 +3,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
-
 import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
